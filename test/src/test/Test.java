82a95670eb6edfb7cb4a7a10039a66c87518da38
rikyardi <riky.ardi321@gmail.com>
2020-06-15 21:06:56 +0700

    nyobalagi.java

--- test/src/test/Test.java
+++ test/src/test/Test.java
@@ -21,19 +21,6 @@ public class Test {
      */
     public static void main(String[] args) {
  
-      //Getting the current date value
-      LocalDate currentdate = LocalDate.now();
-      System.out.println("Current date: "+currentdate);
-      //Getting the current day
-      int currentDay = currentdate.getDayOfMonth();
-      System.out.println("Current day: "+currentdate);
-      //Getting the current month
-      Month currentMonth = currentdate.getMonth();
-      System.out.println("Current month: "+currentMonth);
-      //getting the current year
-      int currentYear = currentdate.getYear();
-      System.out.println("Current month: "+currentYear);
-      
       
       
       //Java calendar in default timezone and default locale
