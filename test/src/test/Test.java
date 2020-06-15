3e0abfd4f5209e47c7b9bd25aa66d3bb9ff8daa1
rikyardi <riky.ardi321@gmail.com>
2020-06-15 21:02:24 +0700

    nyoba

--- test/src/test/Test.java
+++ test/src/test/Test.java
@@ -21,18 +21,18 @@ public class Test {
      */
     public static void main(String[] args) {
  
-//      //Getting the current date value
-//      LocalDate currentdate = LocalDate.now();
-//      System.out.println("Current date: "+currentdate);
-//      //Getting the current day
-//      int currentDay = currentdate.getDayOfMonth();
-//      System.out.println("Current day: "+currentDay);
-//      //Getting the current month
-//      Month currentMonth = currentdate.getMonth();
-//      System.out.println("Current month: "+currentMonth);
-//      //getting the current year
-//      int currentYear = currentdate.getYear();
-//      System.out.println("Current month: "+currentYear);
+      //Getting the current date value
+      LocalDate currentdate = LocalDate.now();
+      System.out.println("Current date: "+currentdate);
+      //Getting the current day
+      int currentDay = currentdate.getDayOfMonth();
+      System.out.println("Current day: "+currentdate);
+      //Getting the current month
+      Month currentMonth = currentdate.getMonth();
+      System.out.println("Current month: "+currentMonth);
+      //getting the current year
+      int currentYear = currentdate.getYear();
+      System.out.println("Current month: "+currentYear);
       
       
       
