6dbbed14d2349bfbd2fd89e193e5b494797806b3
dafimz <dafimuammar31@gmail.com>
2020-06-30 16:00:25 +0700

    update bpjs

--- bpjs.sql
+++ bpjs.sql
@@ -1,13 +1,15 @@
 -- phpMyAdmin SQL Dump
--- version 4.5.1
--- http://www.phpmyadmin.net
+-- version 4.8.5
+-- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: 19 Jun 2020 pada 12.08
--- Versi Server: 10.1.13-MariaDB
--- PHP Version: 5.6.20
+-- Generation Time: Jun 30, 2020 at 10:58 AM
+-- Server version: 10.1.38-MariaDB
+-- PHP Version: 7.3.2
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -23,7 +25,7 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `data_diri`
+-- Table structure for table `data_diri`
 --
 
 CREATE TABLE `data_diri` (
@@ -35,19 +37,22 @@ CREATE TABLE `data_diri` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data untuk tabel `data_diri`
+-- Dumping data for table `data_diri`
 --
 
 INSERT INTO `data_diri` (`nik`, `nomor_kk`, `nama`, `alamat`, `ttl`) VALUES
 ('17238239475839402', '32859384728392814', 'Agus Winarto', 'Jl. Rawarontek RT. 03/01, Kel. Sukamakmur, Kec. Sukamandi, Kab. Jonggol, Jawa Barat', 'Jonggol, 07-08-1999'),
-('32010114030000192', '1803015053', 'Riky Ardiansyah', 'Jl. Amanah RT. 03/08 No.106 Kel. Cibinong, Kec. Cibinong, Kab. Bogor, Jawa Barat ', 'Bogor, 14-03-2000'),
+('180301519000000001', '18800022222211', 'Fadwah Rara', 'Jl. Saibun RT.004/001 No.10 Cijantung Pasar Rebo Jakarta Timur', 'Jakarta, 14-04-2000'),
+('180301519000000221', '18800022222211', 'Dafi Mu\'ammar Zulfikar', 'Jl. Saibun RT.004/001 No.10 Cijantung Pasar Rebo Jakarta Timur', 'Jakarta, 02-12-2000'),
+('3201011403000019', '1803015053', 'Riky Ardiansyah', 'Jl. Amanah RT. 03/08 No.106 Kel. Cibinong, Kec. Cibinong, Kab. Bogor, Jawa Barat ', 'Bogor, 14-03-2000'),
 ('54738473827364183', '46274836193847162', 'Ade Wahyudi', 'Jl. Halimun Ketir RT.03/01 No. 66 Kab. Bogor, Jawa Barat', 'Bogor, 31-04-2000'),
-('57381928847382948', '1803015053', 'Riski Pratama Putra', 'Jl. Amanah RT. 03/08 No.106 Kel. Cibinong, Kec. Cibinong, Kab. Bogor, Jawa Barat ', 'Bogor, 24-03-1990');
+('57381928847382948', '1803015053', 'Riski Pratama Putra', 'Jl. Amanah RT. 03/08 No.106 Kel. Cibinong, Kec. Cibinong, Kab. Bogor, Jawa Barat ', 'Bogor, 24-03-1990'),
+('73856391224835712', '1803015053', 'Sarmili', 'Jl. Vasopati RT. 06/01 Kec. Jayakarta, Kab. Bandungan, Jawa Barat', 'Bogor, 12-12-1912');
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `kartu`
+-- Table structure for table `kartu`
 --
 
 CREATE TABLE `kartu` (
@@ -60,19 +65,22 @@ CREATE TABLE `kartu` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data untuk tabel `kartu`
+-- Dumping data for table `kartu`
 --
 
 INSERT INTO `kartu` (`nomor_kartu`, `nik`, `faskes`, `status`, `kelas`, `terakhir_bayar`) VALUES
-('0002743817274', '57381928847382948', 'Klinik Noordin Wahid', 'Mandiri', '3', '2020-04-30'),
-('0002747141662', '3201011403000019', 'Klinik Noordin Wahid', 'Mandiri', '3', '2020-04-30'),
-('1245323554323', '17238239475839402', 'Klinik Sukamaju', 'Mandiri', '1', '2019-03-21'),
-('6473817236473', '54738473827364183', 'Klinik Mahasta', 'PBI', '0', '2020-05-06');
+('0002743817274', '57381928847382948', 'Klinik Noordin Wahid', 'Mandiri', '3', '2020-06-20'),
+('0002747141662', '3201011403000019', 'Klinik Noordin Wahid', 'Mandiri', '3', '2020-06-20'),
+('083819035315', '180301519000000001', 'Klinik Medical Lillah', 'PBI', '0', ''),
+('1245323554323', '17238239475839402', 'Klinik Sukamaju', 'Mandiri', '1', '2020-06-29'),
+('1803151900111', '180301519000000221', 'Klinik Medical Lillah', 'PBI', '0', ''),
+('6473817236473', '54738473827364183', 'Klinik Mahasta', 'PBI', '0', ''),
+('839378563883468', '73856391224835712', 'Klinik Noordin Wahid', 'Mandiri', '3', '2020-06-20');
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `kelas_mandiri`
+-- Table structure for table `kelas_mandiri`
 --
 
 CREATE TABLE `kelas_mandiri` (
@@ -81,7 +89,7 @@ CREATE TABLE `kelas_mandiri` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data untuk tabel `kelas_mandiri`
+-- Dumping data for table `kelas_mandiri`
 --
 
 INSERT INTO `kelas_mandiri` (`kelas`, `iuran_perbulan`) VALUES
@@ -92,7 +100,7 @@ INSERT INTO `kelas_mandiri` (`kelas`, `iuran_perbulan`) VALUES
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `peserta`
+-- Table structure for table `peserta`
 --
 
 CREATE TABLE `peserta` (
@@ -109,7 +117,7 @@ CREATE TABLE `peserta` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data untuk tabel `peserta`
+-- Dumping data for table `peserta`
 --
 
 INSERT INTO `peserta` (`nomor_kartu`, `nama`, `alamat`, `ttl`, `nomor_kk`, `nik`, `faskes`, `status`, `kelas`, `pembayaran`) VALUES
@@ -144,6 +152,7 @@ ALTER TABLE `kelas_mandiri`
 --
 ALTER TABLE `peserta`
   ADD PRIMARY KEY (`nomor_kartu`);
+COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
